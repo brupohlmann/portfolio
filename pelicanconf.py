501134f1d9a6816479f0ac8d3df525a05f53c420
brupohlmann <brupohlmann@gmail.com>
2018-06-03 17:10:29 -0300

    atualizacao

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'America/Sao_Paulo'
 
 DEFAULT_LANG = 'pt'
 
+
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -22,14 +24,16 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 1
+
+
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
